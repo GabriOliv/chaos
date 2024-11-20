8f1bd651b03bdf151e684d1ed5a8d22a261535d9
Gabriel Laureano <gabriel.oliveira@simeon.com.br>
2024-11-20 18:55:25 -0300

    Create function chaos to return values

--- spec/chaos_spec.cr
+++ spec/chaos_spec.cr
@@ -22,4 +22,63 @@ describe Chaos do
     chaos.reset_probability
     chaos.probability.should eq(0.5)
   end
+
+  # chaos return with Nil
+  it "returns a random value of type nil" do
+    chaos = Chaos::Chaos.new
+    chaos.chaos(nil).should eq(nil)
+  end
+
+  # chaos return with Bool
+  it "returns a correct value of type bool" do
+    chaos = Chaos::Chaos.new
+    chaos.set_probability(0.0)
+    chaos.chaos(true).should be_true
+  end
+
+  it "returns a chaos value of type bool" do
+    chaos = Chaos::Chaos.new
+    chaos.set_probability(1.0)
+    chaos.chaos(true).should be_false
+  end
+
+  # chaos return with Integer int8
+  # chaos return with Integer int16
+  # chaos return with Integer int32
+  # chaos return with Integer int64
+  # chaos return with Integer int128
+  # chaos return with Integer uint8
+  # chaos return with Integer uint16
+  # chaos return with Integer uint32
+  # chaos return with Integer uint64
+  # chaos return with Integer uint128
+  # chaos return with Float float32
+  # chaos return with Float float64
+  # chaos return with Char
+
+  # chaos return with String
+  it "returns a correct value of type string" do
+    chaos = Chaos::Chaos.new
+    chaos.set_probability(0.0)
+    chaos.chaos("abcdefghi").should eq("abcdefghi")
+  end
+
+  it "returns a chaos value of type string" do
+    chaos = Chaos::Chaos.new
+    chaos.set_probability(1.0)
+    chaos.chaos("abcdefghi").should eq("ihgfedcba")
+  end
+
+  # chaos return with Symbol
+  # chaos return with Array
+  # chaos return with Array-like
+  # chaos return with Hash
+  # chaos return with Hash-like
+  # chaos return with Range
+  # chaos return with Regex
+  # chaos return with Tuple
+  # chaos return with NamedTuple
+  # chaos return with Proc
+  # chaos return with Command
+
 end
--- src/chaos.cr
+++ src/chaos.cr
@@ -56,5 +56,80 @@ module Chaos
     def reset_probability
       @probability = DEFAULT_PROBABILITY
     end
+
+    # Nil
+    def chaos(variable : Nil) : Nil
+      variable
+    end
+
+    # Returns a chaotic boolean value based on the given probability.
+    #
+    # This method takes a `Bool` variable as input and returns the same variable unless
+    # a random value generated is less than the current chaos probability, in which case
+    # it returns the opposite value of the input `variable`.
+    #
+    # Example:
+    #
+    # ```
+    # chaos = Chaos::Chaos.new
+    # chaos.set_probability(0.8)
+    # chaos.chaos(true) # => random value of type Bool based on the probability
+    # ```
+    def chaos(variable : Bool) : Bool
+      rand < @probability ? !variable : variable
+    end
+
+    # Integer int8
+
+    # Integer int16
+
+    # Integer int32
+
+    # Integer int64
+
+    # Integer int128
+
+    # Integer uint8
+
+    # Integer uint16
+
+    # Integer uint32
+
+    # Integer uint64
+
+    # Integer uint128
+
+    # Float float32
+
+    # Float float64
+
+    # Char
+
+    # String
+    def chaos(variable : String) : String
+      rand < @probability ? variable.reverse : variable
+    end
+
+    # Symbol
+
+    # Array
+
+    # Array-like
+
+    # Hash
+
+    # Hash-like
+
+    # Range
+
+    # Regex
+
+    # Tuple
+
+    # NamedTuple
+
+    # Proc
+
+    # Command
   end
 end
