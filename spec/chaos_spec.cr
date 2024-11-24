6642dbe3f55068f055ba275bb7430f8c3bce1cac
Gabriel Laureano <gabriel.oliveira@simeon.com.br>
2024-11-24 06:31:06 -0300

    Refactor Chaos class to use nested describe blocks for better organization

--- spec/chaos_spec.cr
+++ spec/chaos_spec.cr
@@ -1,25 +1,29 @@
 require "./spec_helper"
 
 describe Chaos do
-  it "has a version number" do
-    Chaos::VERSION.should_not be_nil
+  describe "version" do
+    it "has a version number" do
+      Chaos::VERSION.should_not be_nil
+    end
   end
 
-  it "initializes with default probability" do
-    chaos = Chaos::Chaos.new
-    chaos.probability.should eq(0.5)
-  end
+  describe "property - probability" do
+    it "initializes with default probability" do
+      chaos = Chaos::Chaos.new
+      chaos.probability.should eq(0.5)
+    end
 
-  it "changes the probability" do
-    chaos = Chaos::Chaos.new
-    chaos.probability = 0.8
-    chaos.probability.should eq(0.8)
-  end
+    it "changes the probability" do
+      chaos = Chaos::Chaos.new
+      chaos.probability = 0.8
+      chaos.probability.should eq(0.8)
+    end
 
-  it "resets the probability to default" do
-    chaos = Chaos::Chaos.new
-    chaos.probability = 0.8
-    chaos.reset_probability
-    chaos.probability.should eq(0.5)
+    it "resets the probability to default" do
+      chaos = Chaos::Chaos.new
+      chaos.probability = 0.8
+      chaos.reset_probability
+      chaos.probability.should eq(0.5)
+    end
   end
 end
